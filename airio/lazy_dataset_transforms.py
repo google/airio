7fa1de6f65cffc0e150a1e5e08ff2f612a1ee752
Gaurav Mishra <mishragaurav@google.com>
2023-09-14 18:10:02 -0700

    Add shard and concat lazy_dataset transforms.
    PiperOrigin-RevId: 565531188

---
+++ airio/lazy_dataset_transforms.py
@@ -0,0 +1,135 @@
+# Copyright 2023 The AirIO Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""PyGrain LazyDataset transformations required for AirIO Task and Mixture.
+
+Most, if not all, of these should be upstreamed into the grain codebase.
+"""
+import bisect
+import dataclasses
+import itertools
+from typing import Sequence, TypeVar
+from absl import logging
+import grain.python as grain
+import numpy as np
+
+
+lazy_dataset = grain.experimental.lazy_dataset
+T = TypeVar("T")
+
+
+# TODO(b/300282178): This is from grain/_src/core/transforms. Merge back there.
+def even_split(
+    num_examples: int, options: grain.ShardOptions
+) -> tuple[int, int]:
+  """Returns the interval for the shard when sharding `num_examples` evenly.
+
+  This splits the interval [0, num_examples - 1] into `shard_count` intervals
+  and returns the `shard_index`'s interval. If `drop_remainder` is True all
+  intervals will have the same size.
+
+  Args:
+    num_examples: Number of examples to shard.
+    options: Options for sharding the data in this process.
+
+  Returns:
+    Tuple with the start and end of the interval. The start is the first
+    example that should be included in this interval and end - 1 is the last
+    example to be include in the shard.
+  """
+  examples_per_shard = num_examples // options.shard_count
+  shard_start = examples_per_shard * options.shard_index
+  shard_end = examples_per_shard * (options.shard_index + 1)
+
+  # Handle remaining examples.
+  num_unused_examples = num_examples % options.shard_count
+
+  if num_unused_examples > 0:
+    if options.drop_remainder:
+      logging.warning(
+          "Dropping %d examples of %d examples (shard %d).",
+          num_unused_examples,
+          num_examples,
+          options.shard_count,
+      )
+    else:
+      shard_start += min(options.shard_index, num_unused_examples)
+      shard_end += min(options.shard_index + 1, num_unused_examples)
+  return shard_start, shard_end
+
+
+# TODO(b/300282178): These should upstreamed to the grain codebase.
+@dataclasses.dataclass(frozen=False)
+class ShardLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
+  """Shards a LazyMapDataset based on provided shard options."""
+
+  parent: lazy_dataset.LazyMapDataset[T]
+  shard_options: grain.ShardOptions
+
+  def __post_init__(self):
+    self._start, self._end = even_split(len(self.parent), self.shard_options)
+
+  @property
+  def sparse(self) -> bool:
+    return self.parent.sparse
+
+  def __len__(self) -> int:
+    return self._end - self._start  # pytype: disable=unsupported-operands
+
+  def __getitem__(self, index: int) -> T | None:
+    epoch = index // len(self)
+    index_in_epoch = index % len(self)
+    index = epoch * len(self.parent) + index_in_epoch + self._start  # pytype: disable=unsupported-operands
+    return self.parent[index]
+
+
+@dataclasses.dataclass(frozen=False)
+class ConcatLazyMapDataset(lazy_dataset.LazyMapDataset[T]):
+  """Concats LazyMapDatasets."""
+
+  parents: Sequence[lazy_dataset.LazyMapDataset[T]]
+
+  @property
+  def sparse(self) -> bool:
+    return any(p.sparse for p in self.parents)
+
+  def __post_init__(self):
+    self._accumulated_lens = [0] + list(
+        itertools.accumulate([len(p) for p in self.parents])
+    )
+
+  def __len__(self) -> int:
+    return sum(len(p) for p in self.parents)
+
+  def __getitem__(self, index: int) -> T | None:
+    parent_index = bisect.bisect_right(self._accumulated_lens, index) - 1  # pytype: disable=wrong-arg-types
+    local_index = index - self._accumulated_lens[parent_index]  # pytype: disable=unsupported-operands
+    return self.parents[parent_index][local_index]
+
+
+class MixedLazyMapDataset(lazy_dataset.MixedLazyMapDataset[T]):
+  """LazyDataset for mixtures with a few added features."""
+
+  def __init__(
+      self,
+      parents: Sequence[lazy_dataset.LazyMapDataset[T]],
+      proportions: Sequence[float | int] | None = None,
+      stop_on_empty_dataset: bool = True,
+  ):
+    super().__init__(parents, proportions)
+    if stop_on_empty_dataset:
+      lengths = np.asarray([len(p) for p in parents])
+      float_proportions = np.asarray(proportions) / sum(proportions)
+      # Stop sampling once any constituent is exhausted.
+      self._length = int((lengths / float_proportions).min())
---
+++ airio/lazy_dataset_transforms_test.py
@@ -0,0 +1,139 @@
+# Copyright 2023 The AirIO Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for lazy_dataset transforms."""
+
+import itertools
+from typing import Sequence
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from airio import lazy_dataset_transforms
+import grain.python as grain
+
+lazy_dataset = grain.experimental.lazy_dataset
+
+
+class ShardLazyMapDatasetTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.data_len = 20
+    self.range_ds = lazy_dataset.RangeLazyMapDataset(self.data_len)
+    self.range_py_list = list(range(self.data_len))
+
+  @parameterized.parameters(
+      (grain.ShardOptions(0, 1), 20),
+      (grain.ShardOptions(0, 2), 10),
+      (grain.ShardOptions(1, 2), 10),
+      (grain.ShardOptions(0, 3), 7),
+      (grain.ShardOptions(1, 3), 7),
+      (grain.ShardOptions(2, 3), 6),
+  )
+  def test_len(self, shard_options: grain.ShardOptions, expected_len: int):
+    range_ds_for_process = lazy_dataset_transforms.ShardLazyMapDataset(
+        self.range_ds,
+        shard_options=shard_options,
+    )
+    self.assertLen(range_ds_for_process, expected_len)
+
+  @parameterized.parameters(itertools.combinations(range(20), 2))
+  def test_getitem(self, shard_id: int, num_shards: int):
+    shard_options = grain.ShardOptions(shard_id, num_shards)
+    start, stop = lazy_dataset_transforms.even_split(
+        self.data_len, shard_options
+    )
+    expected = self.range_py_list[start:stop]
+    ds = lazy_dataset_transforms.ShardLazyMapDataset(
+        self.range_ds, shard_options=shard_options
+    )
+    actual = [ds[i] for i in range(len(ds))]
+    self.assertSequenceEqual(actual, expected)
+
+  @parameterized.parameters(itertools.combinations(range(20), 2))
+  def test_iter(self, shard_id: int, num_shards: int):
+    shard_options = grain.ShardOptions(shard_id, num_shards)
+    start, stop = lazy_dataset_transforms.even_split(
+        self.data_len, shard_options
+    )
+    expected = self.range_py_list[start:stop]
+    ds = lazy_dataset_transforms.ShardLazyMapDataset(
+        self.range_ds, shard_options=shard_options
+    )
+    ds_iter = iter(ds)
+    actual = list(ds_iter)
+    self.assertSequenceEqual(actual, expected)
+
+
+class ConcatLazyMapDatasetTest(parameterized.TestCase):
+
+  @parameterized.parameters(
+      ([3, 2],),
+      ([1, 2, 3],),
+      ([4, 5, 4, 1],),
+  )
+  def test_len(self, parent_lens: Sequence[int]):
+    datasets = []
+    for data_len in parent_lens:
+      datasets.append(lazy_dataset.RangeLazyMapDataset(data_len))
+
+    ds = lazy_dataset_transforms.ConcatLazyMapDataset(
+        datasets
+    )
+    self.assertLen(ds, sum(parent_lens))
+
+  def test_getitem(self):
+    parents = [
+        lazy_dataset.RangeLazyMapDataset(5),
+        lazy_dataset.RangeLazyMapDataset(5, 9),
+        lazy_dataset.RangeLazyMapDataset(9, 18),
+        lazy_dataset.RangeLazyMapDataset(18, 19),
+        lazy_dataset.RangeLazyMapDataset(19, 22),
+    ]
+    ds = lazy_dataset_transforms.ConcatLazyMapDataset(
+        parents
+    )
+    actual = [ds[i] for i in range(len(ds))]
+    self.assertSequenceEqual(actual, range(22))
+
+  def test_iter(self):
+    parents = [
+        lazy_dataset.RangeLazyMapDataset(5),
+        lazy_dataset.RangeLazyMapDataset(5, 9),
+        lazy_dataset.RangeLazyMapDataset(9, 18),
+        lazy_dataset.RangeLazyMapDataset(18, 19),
+        lazy_dataset.RangeLazyMapDataset(19, 22),
+    ]
+    ds = lazy_dataset_transforms.ConcatLazyMapDataset(
+        parents
+    )
+    ds_iter = iter(ds)
+    actual = list(ds_iter)
+    self.assertSequenceEqual(actual, range(22))
+
+
+class MixLazyMapDatasetTest(absltest.TestCase):
+  def test_stopping_on_empty(self):
+    ds1 = lazy_dataset.RangeLazyMapDataset(5)
+    ds2 = lazy_dataset.RangeLazyMapDataset(5, 10)
+    mixed_lzds = lazy_dataset_transforms.MixedLazyMapDataset(
+        parents=[ds1, ds2], proportions=[2, 1]
+    )
+    actual_values = [mixed_lzds[i] for i in range(len(mixed_lzds))]
+    expected_values = [0, 1, 5, 2, 3, 6, 4]
+    self.assertListEqual(expected_values, actual_values)
+
+
+if __name__ == "__main__":
+  absltest.main()
