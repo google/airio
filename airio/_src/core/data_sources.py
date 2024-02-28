613c6bdfaee4544bf477c335b9f5416a779c3f2e
Michelle Casbon <michellecasbon@google.com>
2024-02-28 10:28:39 -0800

    Move TfdsDataSource into the pygrain package.
    PiperOrigin-RevId: 611147988

--- airio/_src/core/data_sources.py
+++ airio/_src/core/data_sources.py
@@ -15,10 +15,9 @@
 """Data Source implementations for AirIO."""
 
 import typing
-from typing import Iterable, Mapping, Protocol, Union
+from typing import Iterable, Protocol
 
 import numpy as np
-import tensorflow_datasets as tfds
 
 
 @typing.runtime_checkable
@@ -68,46 +67,3 @@ class FunctionDataSource(DataSource):
     if split not in self.splits:
       raise ValueError(f'Split {split} not found in {self.splits}.')
     return self._dataset_fn(split=split).size
-
-
-class TfdsDataSource(DataSource):
-  """Wrapper for tfds.data_source with multiple splits support."""
-
-  def __init__(
-      self,
-      tfds_name: str,
-      tfds_data_dir: str | None = None,
-      splits: Union[Iterable[str], Mapping[str, str]] | None = None,
-      decoders: tfds.typing.TreeDict[tfds.decode.Decoder] | None = None,
-  ):
-    self._tfds_name = tfds_name
-    self._tfds_data_dir = tfds_data_dir
-    self._decoders = decoders
-
-    if splits and isinstance(splits, str):
-      self.splits = {splits}
-    else:
-      self.splits = splits or []
-
-    self._sources = {}
-    for split in self.splits:
-      self._sources[split] = tfds.data_source(
-          self._tfds_name,
-          data_dir=self._tfds_data_dir,
-          split=split,
-          decoders=self._decoders,
-      )
-
-  def get_data_source(self, split: str):
-    if split not in self._sources:
-      raise ValueError(
-          f'Split {split} not found in {self.splits} for {self._tfds_name}.'
-      )
-    return self._sources[split]
-
-  def num_input_examples(self, split: str) -> int:
-    if split not in self._sources:
-      raise ValueError(
-          f'Split {split} not found in {self.splits} for {self._tfds_name}.'
-      )
-    return len(self._sources[split])
